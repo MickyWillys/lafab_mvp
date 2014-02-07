bbf82c01ed353953142642e582511f6501811927
MickyWillys <fozeum@yahoo.fr>
2014-02-07 20:02:00 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LafabMvp::Application.config.secret_key_base = '8adb06fbc3d8687bf6c7952dd119d78ba93632e52c792f95815f1c5b2c5cc05fe42ffb65bf17b7dc8559ac1b86a671ce6f3e3133ebfdec0b1c1658da0ef66cd9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LafabMvp::Application.config.session_store :cookie_store, key: '_Lafab_mvp_session'
