ef96206b35c19ab2032924970cec062a292dbc4a
MickyWillys <fozeum@yahoo.fr>
2014-02-08 11:52:28 +0100

    sign out working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+	def show
+	  	@user = current_user		
+	end
+	def update
+		@user = current_user
+    	if @user.update_attributes(user_params)
+      		flash[:success] = "Profile updated"
+      		redirect_to @user
+    	end
+    end
 end
+
+private
+	def current_user
+		@account = current_account
+	  	@user = User.find(@account.user_id)	
+	end 
+	def user_params
+	  params.require(:user).permit(:name)
+	end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,28 @@
 class WelcomeController < ApplicationController
   def index
+  	if signup
+  		@account = current_account
+  		@user = User.create(name: "")
+  		@account.user_id = @user.id
+  		@account.save
+  	end
+
+
   	if account_signed_in? then
 	  	@account = current_account
 	  	# binding.pry
-	  	@user = User.find(@account.user_id)
+	  	@user = User.find(@account.user_id) unless signup
 	end
+  end
+
+  
+
+  def signup
+  	if current_account != nil
+  		@account = current_account 
+  		@account.user_id == nil 
+  	else
+  		signup = false
+  	end
   end
 end
