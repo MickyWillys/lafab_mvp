972e023131cf986a7b8af1086462e7c3b080fd65
MickyWillys <fozeum@yahoo.fr>
2014-02-20 19:17:46 +0100

    home page start

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ LafabMvp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
+  get 'home' => 'welcome#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
