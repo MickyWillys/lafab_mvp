961928b5fe3ed390afe1e6195669417d3ad4eb5b
MickyWillys <fozeum@yahoo.fr>
2014-03-10 16:10:12 +0100

    before activity

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
  
   # GET /message/new
   def new
-    # @user = User.find(params[:user])
+    @user = User.find(params[:user])
     @message = current_user.messages.new
   end
  
